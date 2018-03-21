b6e212c8e3764be675ff0188eb6f0813abdc3b45
Emi Clark <emiko.clark@gmail.com>
2018-03-21 14:06:56 -0400

    refactoring

--- GoogleCoordApi/GoogleCoordApi/GoogleApiClient.swift
+++ GoogleCoordApi/GoogleCoordApi/GoogleApiClient.swift
@@ -12,8 +12,8 @@ class GoogleApiClient {
     
     static var lat: Double?
     static var long: Double?
+    static var vc = ViewController()
     
-//    class func getCoordJson(address: String, completion: @escaping([String:Any])->()) {
     class func getCoordJson(address: String, completion: @escaping(Double,Double)->()) {
 
         let addressConverted = address.replacingOccurrences(of: " ", with: "+", options: .literal, range: nil)
@@ -21,11 +21,11 @@ class GoogleApiClient {
         
         let urlString = "https://maps.googleapis.com/maps/api/geocode/json?address=\(addressConverted)&key=\(secrets.googleApiKey)"
         
-        guard let urlConverted = URL(string: urlString) else {print("url failed"); return}
+        guard let urlConverted = URL(string: urlString) else {print("url failed");  return}
         
         URLSession.shared.dataTask(with: urlConverted) { (data, response, error) in
             
-            guard let data = data else {print("data nil"); return }
+            guard let data = data else {print("data nil"); vc.errorFlag = true; return }
             
             let jsonDict = try? JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
             guard let json = jsonDict else { print("json conversion failed"); return }
@@ -35,11 +35,11 @@ class GoogleApiClient {
             
             guard let firstElement = resultsArr.first as? [String:Any] else { print("failed"); return }
             guard let geometryDict = firstElement["geometry"] as? [String: Any] else { print("gemometry failed"); return }
-            guard let locationDict = geometryDict["location"] as? [String:Any] else {print("location failed"); return }
+            guard let locationDict = geometryDict["location"] as? [String:Any] else {print("location failed"); vc.errorFlag = true; return }
             
             guard let latcoord = locationDict["lat"] as? Double,
                 let longcoord = locationDict["lng"] as? Double
-                else {print("location failed"); return }
+                else {print("location failed");vc.errorFlag = true; return }
             
             lat = latcoord
             long = longcoord
--- GoogleCoordApi/GoogleCoordApi/ViewController.swift
+++ GoogleCoordApi/GoogleCoordApi/ViewController.swift
@@ -13,21 +13,31 @@ class ViewController: UIViewController {
     var gac = GoogleApiClient()
     
     @IBOutlet weak var addressTextField: UITextField!
-
+    @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var latLabel: UILabel!
     @IBOutlet weak var longLabel: UILabel!
-    
-    
+    var errorFlag: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let address = "27+e+13th+Street,+NY,+NY+10003"
-        GoogleApiClient.getCoordJson(address: address) { (lat,long) in
-            print(lat, long)
-        }
+        errorLabel.isHidden = true
     }
-
+    
     @IBAction func convertButtonTapped(_ sender: UIButton) {
+        self.errorLabel.isHidden = true
+        guard let address = self.addressTextField.text else {print("Address field failed-nil"); self.errorFlag = true; return }
+        
+        GoogleApiClient.getCoordJson(address: address) { (lat,long) in
+            if  self.errorFlag {
+                self.errorLabel.isHidden = false
+            } else {
+
+                DispatchQueue.main.async {
+                    self.latLabel.text = "\(lat)"
+                    self.longLabel.text = "\(long)"
+                }
+            }
+        }
     }
 }
 
