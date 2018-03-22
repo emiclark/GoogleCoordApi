0e8d3355b6a884ee67781eedbbfde188be7f4043
saraahmad1114 <asara1114@gmail.com>
2018-03-22 16:57:15 -0400

    changes

--- GoogleCoordApi/GoogleCoordApi/GoogleApiClient.swift
+++ GoogleCoordApi/GoogleCoordApi/GoogleApiClient.swift
@@ -23,32 +23,32 @@ class GoogleApiClient {
         
         URLSession.shared.dataTask(with: urlConverted) { (data, response, error) in
             
-            guard let data = data else {print("data nil"); return }
+            guard let unwrappedError = error else {print("did not unwrap"); return}
 
-            
             do {
-            let jsonDict = try? JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-            
-            guard let json = jsonDict else { print("json conversion failed"); return }
-            guard let resultsArr = json["results"] as? Array<Any> else {print("results array failed"); return }
-            
-            guard let firstElement = resultsArr[0] as? [String:Any] else { print("failed"); return }
-            guard let geometryDict = firstElement["geometry"] as? [String: Any] else { print("gemometry failed"); return }
-            guard let locationDict = geometryDict["location"] as? [String:Any] else {print("location failed"); return }
-            
-            guard let latcoord = locationDict["lat"] as? Double,
-                let longcoord = locationDict["lng"] as? Double
-                else {print("error while unwrapping lat and lng coordinates"); return}
-    
-            self.lat = latcoord
-            self.long = longcoord
-            
-            completion(self.lat!,self.long!)
+                if let data = data {
+                let jsonDict = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                guard let json = jsonDict as? [String: Any] else { print("json conversion failed"); return }
+                guard let resultsArr = json["results"] as? Array<Any> else {print("results array failed"); return }
+                
+                guard let firstElement = resultsArr[0] as? [String:Any] else { print("failed"); return }
+                guard let geometryDict = firstElement["geometry"] as? [String: Any] else { print("gemometry failed"); return }
+                guard let locationDict = geometryDict["location"] as? [String:Any] else {print("location failed"); return }
+                
+                guard let latcoord = locationDict["lat"] as? Double,
+                     let longcoord = locationDict["lng"] as? Double
+                     else {print("error while unwrapping lat and lng coordinates"); return}
+                
+                self.lat = latcoord
+                self.long = longcoord
+                
+                completion(self.lat!,self.long!)
+                }
             }
-            
-            catch
-            {
-                print(error)
+                
+            catch{
+                print(unwrappedError)
             }
 
         }.resume()
